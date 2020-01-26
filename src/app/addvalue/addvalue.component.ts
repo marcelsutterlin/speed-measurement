5bdb3c78864cf4db33a5f2d6d025b039014453c5
Marcel Sütterlin <marcelsutterlin@gmail.com>
2020-01-26 16:44:13 +0100

    Added data validation to input

--- src/app/addvalue/addvalue.component.ts
+++ src/app/addvalue/addvalue.component.ts
@@ -7,13 +7,31 @@ import { DataService } from '../data.service'
   styleUrls: ['./addvalue.component.css']
 })
 export class AddvalueComponent implements OnInit {
-  public newvalue: number;
+  public newvalue;
 
 
   public addValue(){
-    this.dataService.addValue(this.newvalue);
+    if (this.newvalue > 0 && this.newvalue < 300 && typeof this.newvalue  == "number"){
+      this.dataService.addValue(this.newvalue);
+    }
+    else if (this.newvalue < 0) {
+      alert("Geschwindigkeit ist unter null.");
+    }
+    else if (this.newvalue > 0) {
+      alert("Geschwindigkeit ist über 299 km/h.");
+    }
+    else if (this.newvalue != "undefined"){
+      alert("Wert ist leer.");
+    }
+    else if (typeof this.newvalue != "number"){
+      alert("Typ der Eingabe ist keine Nummer.");
+    }
+    else {
+      alert("Eingabe ist ungültig.");
+    }
+
   }
-  constructor(  private dataService: DataService) {
+  constructor(private dataService: DataService) {
    }
 
   ngOnInit() {
